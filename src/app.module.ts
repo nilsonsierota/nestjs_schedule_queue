5548bac8f100cedd12556cc216f86ec89d4687aa
Nilson Sierota Correia <nilsoncv2@gmail.com>
2023-02-22 10:15:36 -0400

    fix: package

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,14 @@ import { TweetsModule } from './tweets/tweets.module';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { join } from 'path';
 import { ScheduleModule } from '@nestjs/schedule';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
   imports: [
     ScheduleModule.forRoot(),
+    BullModule.forRoot({
+      redis: { host: 'redis', port: 6379 },
+    }),
     SequelizeModule.forRoot({
       dialect: 'sqlite',
       host: join(__dirname, 'database.sqlite'),
--- src/tweets/tweets.module.ts
+++ src/tweets/tweets.module.ts
@@ -4,9 +4,14 @@ import { TweetsController } from './tweets.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Tweet } from './entities/tweet.entity';
 import { TweetsCountService } from './tweets-count/tweets-count.service';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
-  imports: [CacheModule.register(), SequelizeModule.forFeature([Tweet])],
+  imports: [
+    CacheModule.register(),
+    SequelizeModule.forFeature([Tweet]),
+    BullModule.registerQueue({ name: 'emails' }),
+  ],
   controllers: [TweetsController],
   providers: [TweetsService, TweetsCountService],
 })
